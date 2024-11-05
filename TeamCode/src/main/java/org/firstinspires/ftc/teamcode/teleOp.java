b91e88a8adce4945dc78693d7a01ff24ab1c519c
Mhoyt07 <122686275+Mhoyt07@users.noreply.github.com>
2024-11-04 22:53:32 -0600

    fixed teleop file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Subsystems.DriveSubsystem;
 
 @TeleOp
-public class TeleOp extends LinearOpMode {
+public class teleOp extends LinearOpMode {
 
     public void runOpMode() {
 
